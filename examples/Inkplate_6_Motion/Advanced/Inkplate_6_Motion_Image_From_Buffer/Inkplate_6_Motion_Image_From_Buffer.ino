35a515fbb4a01632404868ef812fb5803cf67000
Borna Biro <43830307+BornaBiro@users.noreply.github.com>
2024-10-30 08:51:46 +0100

    Examples for the Images from SD and from the buffer.

--- examples/Inkplate_6_Motion/Advanced/Inkplate_6_Motion_Image_From_Buffer/Inkplate_6_Motion_Image_From_Buffer.ino
+++ examples/Inkplate_6_Motion/Advanced/Inkplate_6_Motion_Image_From_Buffer/Inkplate_6_Motion_Image_From_Buffer.ino
@@ -1,9 +1,13 @@
 // Include Inkplate Motion Library.
 #include "InkplateMotion.h"
+
+// Include one of the images. To get this image raw data, use imageToHex.py
+// script. This script will convert BMP, JPG or PNG file into .h byte array.
 #include "parrotSolderedBmp.h"
 //#include "parrotSolderedJpg.h"
 //#include "parrotSolderedPng.h"
 
+// Create Inkplate Motion object.,
 Inkplate inkplate;
 
 void setup()
@@ -15,24 +19,30 @@ void setup()
     Serial.println("Hello from Inkplate 6 Motion!");
     Serial.flush();
 
-    // Initialize Inkplate Motion Library.
+    // Initialize Inkplate Motion Library. Use 4 bit color mode.
     inkplate.begin(INKPLATE_GL16);
     inkplate.setTextColor(0, 15);
     inkplate.display();
 
+    // Show message on the screen.
     inkplate.println("loading image from the SRAM, please wait...");
     inkplate.partialUpdate();
 
+    // Use one of the included images.
+    // First parameter is byte array of the image, second is size of the array, third and forth are X and Y, fifth
+    // is the color inversion, sixth is the dither and the last one is image format.
     if (!inkplate.image.drawFromBuffer((uint8_t*)parrotSolderedBmp, sizeof(parrotSolderedBmp), 10, 10, false, 0, INKPLATE_IMAGE_DECODE_FORMAT_BMP))
     //if (!inkplate.image.drawFromBuffer((uint8_t*)parrotSolderedJpg, sizeof(parrotSolderedJpg), 10, 10, false, 0, INKPLATE_IMAGE_DECODE_FORMAT_JPG))
     //if (!inkplate.image.drawFromBuffer((uint8_t*)parrotSolderedPng, sizeof(parrotSolderedPng), 10, 10, false, 0, INKPLATE_IMAGE_DECODE_FORMAT_PNG))
     {
+        // If image decode failed, show error.
         inkplate.println("Image decode failed!");
         inkplate.printf("Decode Err: %d\r\n", inkplate.image.getError());
         inkplate.partialUpdate();
     }
     else
     {
+        // Otherwise, just update the screen.
         inkplate.display();
     }
 }
