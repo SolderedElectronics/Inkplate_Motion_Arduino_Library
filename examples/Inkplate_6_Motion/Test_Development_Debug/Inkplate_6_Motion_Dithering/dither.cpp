174fc9b30ee9814d675ca9b49690c793903aff2b
Borna Biro <43830307+BornaBiro@users.noreply.github.com>
2024-12-18 17:31:02 +0100

    Added doxygen, removed unnecessary code.

--- examples/Inkplate_6_Motion/Test_Development_Debug/Inkplate_6_Motion_Dithering/dither.cpp
+++ examples/Inkplate_6_Motion/Test_Development_Debug/Inkplate_6_Motion_Dithering/dither.cpp
@@ -5,11 +5,12 @@
 
 /* TODO LIST:
  * - [Done] Use 8 bit mode
- * - Optimise the code - procesing under 200ms would be great - can't be done, pixel write is slow as well as the dithering.
+ * - [Done] Optimise the code - procesing under 200ms would be great
+ *      - Can't be done, pixel write is slow as well as the dithering.
  * - [Done] Use DMA for SDRAM buffering.
  * - [Done] Force function for dithering to output data on the current row that is not used anymore after dither has been done.
  * - [Done] Do a general overview and checks
- * - Do a DoxyGen
+ * - [Done] Do a DoxyGen
  * - [Done] Test on different modes - modes should be automatically selected.
  * - [Done] Check allocations in the begin!
  * - Check if code can be more readable.
@@ -34,6 +35,17 @@ ImageProcessing::~ImageProcessing()
     this->freeResources();
 }
 
+/**
+ * @brief   Initializes image processing library.
+ * 
+ * @param   Inkplate *_inkplate
+ *          Inkplate object - used for drawPixel and DMA buffers.
+ * @param   uint16_t _displayWidth
+ *          Real screen width in pixels (without rotation). 
+ * @return  bool
+ *          true - Initialization successful.
+ *          false - Initialization failed. Check memory or input parameters.
+ */
 bool ImageProcessing::begin(Inkplate *_inkplate, uint16_t _displayWidth)
 {
     // Check for the Inkplate object pointer.
@@ -53,10 +65,6 @@ bool ImageProcessing::begin(Inkplate *_inkplate, uint16_t _displayWidth)
     _nextErrorBuffer = (int16_t*)malloc(_errorBufferSize);
     _afterNextErrorBuffer = (int16_t*)malloc(_errorBufferSize);
 
-    // Since is RGB = 24bits or 3 bytes, multiply by three.
-    // Also three rows are buffered due Stucki dither, so multiply by three.
-    //_bufferedRowsArray = (uint8_t*)malloc(_displayW * sizeof(uint8_t) * 3 * 3);
-
     // Check for allocation.
     if ((_errorBuffer == NULL) || (_nextErrorBuffer == NULL) || (_afterNextErrorBuffer == NULL))
     {
@@ -71,6 +79,35 @@ bool ImageProcessing::begin(Inkplate *_inkplate, uint16_t _displayWidth)
     return true;
 }
 
+/**
+ * @brief   Main processing method for the provided framebuffer.
+ * 
+ * @param   uint8_t *_imageBuffer
+ *          Pointer to the address where image is stored. Only works on RGB888 (24bit RGB).
+ * @param   int16_t _x0
+ *          X position where to draw image on the screen.
+ * @param   int16_t _y0
+ *          Y position where to draw image on the screen.
+ * @param   uint16_t _width
+ *          Width of the image stored in the framebuffer (in pixels).
+ * @param   uint16_t _height
+ *          Height of the image stored in the framebuffer (in pixels).
+ * @param   bool _ditheringEnabled
+ *          Switch for disable/enable image dithering.
+ * @param   bool _colorInversion
+ *          Switch for the disable/enable pixel color inversion.
+ * @param   const KernelElement *_ditherKernelParameters
+ *          Pointer to the dither kernel parameters. NULL if dithering is not used.
+ *          Provided Kernels can be used or custom ones.
+ * @param   size_t _ditherKernelParametersSize
+ *          Dither kernels size in bytes.
+ * @param   uint8_t _bitDepth
+ *          Output bit depth - 4 for 4 bit mode, 1 for 1 bit mode.
+ *          Other modes are not supported.
+ * 
+ * @note    Processing is done row-by-row due SDRAM buffering.
+ *          
+ */
 void ImageProcessing::processImage(uint8_t *_imageBuffer, int16_t _x0, int16_t _y0, uint16_t _width, uint16_t _height, bool _ditheringEnabled, bool _colorInversion, const KernelElement *_ditherKernelParameters, size_t _ditherKernelParametersSize, uint8_t _bitDepth)
 {
     // Check for the input parameters.
@@ -110,7 +147,7 @@ void ImageProcessing::processImage(uint8_t *_imageBuffer, int16_t _x0, int16_t _
         // G = 0.7152 => 0.7152 * 256 = 183.0912 = 183
         // B = 0.0722 => 0.0722 * 256 = 18.4832 = 19
         // Tune if needed.
-        this->toGrayscaleRow((uint8_t*)(_inkplatePtr->_dmaBuffer[2]), _width, 54, 183, 19);
+        this->toGrayscaleRow((uint8_t*)(_inkplatePtr->_dmaBuffer[2]), _width);
 
         // Do the inversion if needed.
         if (_colorInversion) this->invertColorsRow((uint8_t*)(_inkplatePtr->_dmaBuffer[2]), _width);
@@ -120,6 +157,27 @@ void ImageProcessing::processImage(uint8_t *_imageBuffer, int16_t _x0, int16_t _
     if (_ditheringEnabled) free(_ditherWeightFactors);
 }
 
+/**
+ * @brief   Process image - convert it into the 8 bit grayscale. Use the same input buffer for the 
+ *          output (overwritte old/original data).
+ * 
+ * @param   uint8_t *_imageBuffer
+ *          Pointer to the address where image is stored. Only works on RGB888 (24bit RGB).
+ * @param   uint16_t _imageWidth
+ *          Size of the image in the buffer (in pixels).
+ * @param   uint8_t _redParameter
+ *          Parameter for the red to grayscale conversion.
+ * @param   uint8_t _greenParameter
+ *          Parameter for the green to grayscale conversion.
+ * @param   uint8_t _blueParameter
+ *          Parameter for the blue to grayscale conversion.
+ * 
+ * @note    Conversion to the grayscale must be done, cannot be disabled in the image processing.
+ *          By default, it's used BT.709 standard for RGB to grayscale conversion.
+ *          R = 0.2126 => 0.2126 * 256 = 54.4256 = 54
+ *          G = 0.7152 => 0.7152 * 256 = 183.0912 = 183
+ *          B = 0.0722 => 0.0722 * 256 = 18.4832 = 19
+ */
 void ImageProcessing::toGrayscaleRow(uint8_t *_imageBuffer, uint16_t _imageWidth, uint8_t _redParameter, uint8_t _greenParameter, uint8_t _blueParameter)
 {
     for (int _x = 0; _x < _imageWidth; _x++)
@@ -137,6 +195,16 @@ void ImageProcessing::toGrayscaleRow(uint8_t *_imageBuffer, uint16_t _imageWidth
     }
 }
 
+/**
+ * @brief   Goes though whole framebuffer and inverts color (by simply inverting the byte).
+ *          Works both on 4 bit and 1 bit mode.
+ * 
+ * @param   uint8_t *_imageBuffer
+ *          Pointer to the framebuffer needs to be processed.
+ * @param   uint16_t _imageWidth
+ *          Width of the image stored in framebuffer.
+ *          
+ */
 void ImageProcessing::invertColorsRow(uint8_t *_imageBuffer, uint16_t _imageWidth)
 {
     // Invert the colors by inverting whole 8 bit color byte.
@@ -147,8 +215,26 @@ void ImageProcessing::invertColorsRow(uint8_t *_imageBuffer, uint16_t _imageWidt
 }
 
 /**
- * @brief Optimize Error Propagation in Dithering
- *        Precompute error distribution weights and reduce unnecessary branching.
+ * @brief   Method dithers one row of the provided 
+ *          
+ * @param   uint8_t *_currentRow
+ *          Pointer to the address of the buffer for the current processed row.
+ * @param   uint8_t *_nextRow
+ *          Pointer to the address of the buffer for the next processed row.
+ * @param   uint8_t *_rowAfterNext
+ *          Pointer to the address of the buffer for the after next processed row.
+ * @param   int16_t _y0
+ *          Y position where the image should be drawn on screen.
+ * @param   uint16_t _width
+ *          Width of the image provided in the buffers (in pixels).
+ * @param   const KernelElement *_ditherKernelParameters
+ *          Pointer to the dither kernel parameters. NULL if dithering is not used.
+ *          Provided Kernels can be used or custom ones.
+ * @param   size_t _ditherKernelParametersSize
+ *          Dither kernels size in bytes.
+ * @param   uint8_t_bitDepth
+ *          Output bit depth - 4 for 4 bit mode, 1 for 1 bit mode.
+ *          Other modes are not supported.
  */
 void ImageProcessing::ditherImageRow(uint8_t *_currentRow, uint8_t *_nextRow, uint8_t *_rowAfterNext, int16_t _y0, uint16_t _width, const KernelElement *_ditherKernelParameters, size_t _ditherKernelParametersSize, uint8_t _bitDepth)
 {
@@ -232,6 +318,18 @@ void ImageProcessing::ditherImageRow(uint8_t *_currentRow, uint8_t *_nextRow, ui
     memset(_afterNextErrorBuffer, 0, _errorBufferSize);
 }
 
+/**
+ * @brief   Method writes the pixels into the epaper/Inkplate main framebuffer.
+ * 
+ * @param   int16_t _x0
+ *          X coridinate where to start writing the image.
+ * @param   int16_t _y0
+ *          Y coridinate where to start writing the image.
+ * @param   uint8_t *_imageBuffer
+ *          Pointer to the framebuffer needs to be processed.
+ * @param   uint16_t _imageWidth
+ *          Width of the image stored in framebuffer.
+ */
 void ImageProcessing::writePixels(int16_t _x0, int16_t _y0, uint8_t *_imageBuffer, uint16_t _imageWidth)
 {
     for (int _xPixel = 0; _xPixel < _imageWidth; _xPixel++)
@@ -240,6 +338,18 @@ void ImageProcessing::writePixels(int16_t _x0, int16_t _y0, uint8_t *_imageBuffe
     }
 }
 
+/**
+ * @brief   Moves the buffers around. Moves them forward and loops back around.
+ *          It goes [1][2][3] -> [2][3][1].
+ * 
+ * @param   uint8_t **_currentRow
+ *          Pointer to the first buffer.
+ * @param   uint8_t **_nextRow
+ *          Pointer to the second buffer.
+ * @param   uint8_t **_rowAfterNext
+ *          Pointer to the three buffer.
+ *          
+ */
 void ImageProcessing::moveBuffers(uint8_t **_currentRow, uint8_t **_nextRow, uint8_t **_rowAfterNext)
 {
     // Move the buffers([1][2][3] -> [2][3][1]).
@@ -249,6 +359,10 @@ void ImageProcessing::moveBuffers(uint8_t **_currentRow, uint8_t **_nextRow, uin
     *_rowAfterNext = _temp;
 }
 
+/**
+ * @brief   Releases the used resources.
+ * 
+ */
 void ImageProcessing::freeResources()
 {
     // Free error buffers.
@@ -263,10 +377,27 @@ void ImageProcessing::freeResources()
     _errorBuffer = NULL;
     _nextErrorBuffer = NULL;
     _afterNextErrorBuffer = NULL;
-    //_bufferedRowsArray = NULL;
     _ditherWeightFactors = NULL;
 }
 
+/**
+ * @brief   Method is used to prepare the buffers for the image processing.
+ *          It preloads, decodes first three rows, precalculates the dither parameters and clears the buffers.
+ * 
+ * @param   uint8_t *_imageFramebuffer
+ *          Pointer to the framebuffer needs to be processed.
+ * @param   uint16_t _w
+ *          Size of the image in the buffer that needs to be processes (in pixels).
+ * @param   bool _ditheringEnabled
+ *          Switch for disable/enable image dithering.
+ * @param   bool _colorInversion
+ *          Switch for the disable/enable pixel color inversion.
+ * @param   const KernelElement *_ditherKernelParameters
+ *          Pointer to the dither kernel parameters. NULL if dithering is not used.
+ *          Provided Kernels can be used or custom ones.
+ * @param   size_t_ditherKernelParametersSize
+ *          Dither kernels size in bytes.
+ */
 void ImageProcessing::prepare(uint8_t *_imageFramebuffer, uint16_t _w,  bool _ditheringEnabled, bool _colorInversion,  const KernelElement *_ditherKernelParameters, size_t _ditherKernelParametersSize)
 {
     // Initialize dither buffers.
